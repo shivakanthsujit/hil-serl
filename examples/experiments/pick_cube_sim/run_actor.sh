a709b18749b22392cb03de331259005304726b01
Shivakanth Sujit <shivakanth.sujit@gmail.com>
2025-02-13 18:17:40 +0900

    Make the ip address of the learner process a cli arg to the bash script

--- examples/experiments/pick_cube_sim/run_actor.sh
+++ examples/experiments/pick_cube_sim/run_actor.sh
@@ -3,8 +3,10 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
 
 exp_name=${1:-"bin_sim"}
 run_name=${2:-"six_run"}
+learner_ip=${3:-"localhost"}
 
 python ../../train_rlpd_sim.py "$@" \
     --exp_name=${exp_name} \
     --checkpoint_path=/data/hil/${exp_name}/${run_name} \
     --actor \
+    --ip=${learner_ip} \
